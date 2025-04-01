ab4e2ed5e574253294c6d9f22f4c1062b6d51f05
Vendissa <venujadissanayake@gmail.com>
2025-04-02 00:48:59 +0530

    3

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path")
 const authRoutes = require("./routes/authRoutes");
 const gameRoutes = require("./routes/gameRoutes");
-const uploadRoutes = require("./routes/uploadRoutes");
+const uploadRoutes = require("./routes/uploadroutes");
 
 const app = express();
 const PORT = 5000;
 
 // Enable CORS for your frontend
 app.use(cors());
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); //to allow frontend requests to send authentication tokens
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json()); // For parsing JSON requests
 
 mongoose.connect("mongodb://localhost:27017/Users");
@@ -19,7 +20,12 @@ mongoose.connect("mongodb://localhost:27017/Users");
 app.use("/auth", authRoutes); // Auth routes are now prefixed with /auth
 app.use("/game", gameRoutes); // Game routes are now prefixed with /game
 app.use("/upload", uploadRoutes); //upload routes are now prefixed with /upload
-app.use("/uploads", express.static("uploads"));
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {
+    setHeaders: (res) => {
+      res.set("Cross-Origin-Resource-Policy", "cross-origin");
+    }
+  }));
 
 
 // Start Server
